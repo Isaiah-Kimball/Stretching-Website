eb58bb1e25190cb4396e49a039d896186a8ba86d
Isaiah-Kimball <biznees09@gmail.com>
2024-07-28 23:54:58 -0500

    Added the toggle feature, bettered the styling

--- stretch.js
+++ stretch.js
@@ -3,3 +3,13 @@ const create = document.querySelectorAll("#create");
 for (const button of create){
     button.addEventListener("click", ()=>{alert("Account creation not finished yet, stay tuned!")})
 }
+
+function toggleVisibility(id){
+    const section = document.getElementById(id);
+    if(section.classList.contains("hidden")){
+        section.classList.remove("hidden");
+    }
+    else{
+        section.classList.add("hidden");
+    }
+}

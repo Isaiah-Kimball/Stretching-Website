09c3c814fb5b39869a191aa2c77104a82405fa0b
Isaiah-Kimball <biznees09@gmail.com>
2024-08-07 01:21:30 -0500

    Changed the checkbox to only exclude stretches that fullfill all of the unchecked boxes

--- stretch.js
+++ stretch.js
@@ -4,12 +4,59 @@ for (const button of create){
     button.addEventListener("click", ()=>{alert("Account creation not finished yet, stay tuned!")})
 }
 
-function toggleVisibility(id){
-    const section = document.getElementById(id);
-    if(section.classList.contains("hidden")){
-        section.classList.remove("hidden");
+let hideClasses = new Set();
+hideClasses.add(".stretch")
+
+function toggleVisibility(classDisable){
+    if (hideClasses.has(classDisable)){
+        console.log("Class has been removed from set");
+        hideClasses.delete(classDisable);
     }
     else{
-        section.classList.add("hidden");
+        console.log("Added new class");
+        hideClasses.add(classDisable);
+    }
+    checkStretches();
+}
+
+function checkStretches(){
+    stretches = document.querySelectorAll(".stretch");
+    //console.log(stretches);
+    for (const stretch of stretches){
+        console.log(stretch);
+        let stretchSet = new Set(stretch.classList);
+        console.log(`stretchSet: ${[...stretchSet]}, hideClasses: ${[...hideClasses]}`);
+        let hide = true;
+
+        for (const clas of stretchSet){
+            console.log(`Current class looking at: ."${clas}`);
+            if (!hideClasses.has("." + clas)){
+                hide = false;
+                break;
+            }
+        }
+
+        if (hide){
+            console.log(`Stretch disabled: ${stretch}`);
+            stretch.style.display = "none";
+        }
+        else{
+            stretch.style.display = "block";
+        }
     }
 }
+
+// Function will more than likely need to be changed
+/*function toggleVisibility(classDisable){
+    const section = document.querySelectorAll(classDisable);
+    console.log(section);
+    for (const stretch of section){
+        if(stretch.classList.contains("hidden")){
+            stretch.classList.remove("hidden");
+        }
+        else{
+            stretch.classList.add("hidden");
+        }
+    }
+    
+}*/
